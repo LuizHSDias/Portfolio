cb5bef29f878fbc14a98003b06d0f4f2ce4d3871
LHSDias <luizhenriquemc15@gmail.com>
2026-07-30 21:06:45 -0300

    Atualizando

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,111 +1,90 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
 
-import Java from "../../Assets/TechIcons/Java.svg";
-import Javascript from "../../Assets/TechIcons/Javascript.svg";
-import Typescript from "../../Assets/TechIcons/Typescript.svg";
-import ReactIcon from "../../Assets/TechIcons/React.svg";
-import Python from "../../Assets/TechIcons/Python.svg";
-import Git from "../../Assets/TechIcons/Git.svg";
-import SQL from "../../Assets/TechIcons/SQL.svg";
-import HTML from "../../Assets/TechIcons/HTML.svg";
-import CSS from "../../Assets/TechIcons/CSS.svg";
-import Docker from "../../Assets/TechIcons/Docker.svg";
-import CSharp from "../../Assets/TechIcons/CSharp.svg";
-import DotNet from "../../Assets/TechIcons/DotNet.svg";
-import Pandas from "../../Assets/TechIcons/Pandas.svg";
-import Numpy from "../../Assets/TechIcons/Numpy.svg";
-import Sklearn from "../../Assets/TechIcons/ScikitLearn.svg";
+import {
+  SiSpringboot,
+  SiAngular,
+  SiReact,
+  SiJavascript,
+  SiTypescript,
+  SiPython,
+  SiHtml5,
+  SiCss3,
+  SiDotnet,
+  SiPandas,
+  SiNumpy,
+} from "react-icons/si";
 
-import { SiAngular, SiSpringboot } from "react-icons/si";
+import { FaJava } from "react-icons/fa6";
+import { TbBrandCSharp } from "react-icons/tb";
 
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
 
       <Col xs={6} md={3} lg={2} className="tech-icons">
-        <img src={Java} alt="Java" className="tech-icon-images" />
+        <FaJava size={45} color="#f89820" />
         <div className="tech-icons-text">Java</div>
       </Col>
 
       <Col xs={6} md={3} lg={2} className="tech-icons">
-        <SiSpringboot size={26} color="#6DB33F" />
+        <SiSpringboot size={45} color="#6DB33F" />
         <div className="tech-icons-text">Spring Boot</div>
       </Col>
 
       <Col xs={6} md={3} lg={2} className="tech-icons">
-        <img src={Python} alt="Python" className="tech-icon-images" />
-        <div className="tech-icons-text">Python</div>
-      </Col>
-
-      <Col xs={6} md={3} lg={2} className="tech-icons">
-        <img src={CSharp} alt="C#" className="tech-icon-images" />
+        <TbBrandCSharp size={45} color="#68217A" />
         <div className="tech-icons-text">C#</div>
       </Col>
 
       <Col xs={6} md={3} lg={2} className="tech-icons">
-        <img src={DotNet} alt=".NET" className="tech-icon-images" />
+        <SiDotnet size={45} color="#512BD4" />
         <div className="tech-icons-text">.NET</div>
       </Col>
 
       <Col xs={6} md={3} lg={2} className="tech-icons">
-        <SiAngular size={26} color="#DD0031" />
+        <SiPython size={45} color="#3776AB" />
+        <div className="tech-icons-text">Python</div>
+      </Col>
+
+      <Col xs={6} md={3} lg={2} className="tech-icons">
+        <SiAngular size={45} color="#DD0031" />
         <div className="tech-icons-text">Angular</div>
       </Col>
 
       <Col xs={6} md={3} lg={2} className="tech-icons">
-        <img src={ReactIcon} alt="React" className="tech-icon-images" />
+        <SiReact size={45} color="#61DAFB" />
         <div className="tech-icons-text">React</div>
       </Col>
 
       <Col xs={6} md={3} lg={2} className="tech-icons">
-        <img src={Typescript} alt="TypeScript" className="tech-icon-images" />
+        <SiTypescript size={45} color="#3178C6" />
         <div className="tech-icons-text">TypeScript</div>
       </Col>
 
       <Col xs={6} md={3} lg={2} className="tech-icons">
-        <img src={Javascript} alt="JavaScript" className="tech-icon-images" />
+        <SiJavascript size={45} color="#F7DF1E" />
         <div className="tech-icons-text">JavaScript</div>
       </Col>
 
       <Col xs={6} md={3} lg={2} className="tech-icons">
-        <img src={HTML} alt="HTML5" className="tech-icon-images" />
+        <SiHtml5 size={45} color="#E34F26" />
         <div className="tech-icons-text">HTML5</div>
       </Col>
 
       <Col xs={6} md={3} lg={2} className="tech-icons">
-        <img src={CSS} alt="CSS3" className="tech-icon-images" />
+        <SiCss3 size={45} color="#1572B6" />
         <div className="tech-icons-text">CSS3</div>
       </Col>
 
       <Col xs={6} md={3} lg={2} className="tech-icons">
-        <img src={SQL} alt="SQL" className="tech-icon-images" />
-        <div className="tech-icons-text">MySQL / SQL</div>
-      </Col>
-
-      <Col xs={6} md={3} lg={2} className="tech-icons">
-        <img src={Docker} alt="Docker" className="tech-icon-images" />
-        <div className="tech-icons-text">Docker</div>
-      </Col>
-
-      <Col xs={6} md={3} lg={2} className="tech-icons">
-        <img src={Git} alt="Git" className="tech-icon-images" />
-        <div className="tech-icons-text">Git</div>
-      </Col>
-
-      <Col xs={6} md={3} lg={2} className="tech-icons">
-        <img src={Pandas} alt="Pandas" className="tech-icon-images" />
+        <SiPandas size={45} color="#150458" />
         <div className="tech-icons-text">Pandas</div>
       </Col>
 
       <Col xs={6} md={3} lg={2} className="tech-icons">
-        <img src={Numpy} alt="NumPy" className="tech-icon-images" />
+        <SiNumpy size={45} color="#013243" />
         <div className="tech-icons-text">NumPy</div>
-      </Col>
-
-      <Col xs={6} md={3} lg={2} className="tech-icons">
-        <img src={Sklearn} alt="Scikit-learn" className="tech-icon-images" />
-        <div className="tech-icons-text">Scikit-learn</div>
       </Col>
 
     </Row>
--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -1,57 +1,47 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
 
-import intelliJ from "../../Assets/TechIcons/intellij-idea.svg";
-import vsCode from "../../Assets/TechIcons/vscode.svg";
-import visualStudio from "../../Assets/TechIcons/Visual Studio.svg";
-import docker from "../../Assets/TechIcons/Docker.svg";
-import Postman from "../../Assets/TechIcons/Postman.svg";
-import Git from "../../Assets/TechIcons/Git.svg";
-import GitHub from "../../Assets/TechIcons/Github.svg";
-import MySQL from "../../Assets/TechIcons/MySQL Workbench.svg";
+import {
+  SiIntellijidea,
+  SiVisualstudiocode,
+  SiDocker,
+  SiPostman,
+  SiGit,
+  SiGithub,
+} from "react-icons/si";
 
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
 
       <Col xs={6} md={3} lg={2} className="tech-icons">
-        <img src={intelliJ} alt="IntelliJ IDEA" className="tech-icon-images" />
+        <SiIntellijidea size={45} color="#000000" />
         <div className="tech-icons-text">IntelliJ IDEA</div>
       </Col>
 
       <Col xs={6} md={3} lg={2} className="tech-icons">
-        <img src={vsCode} alt="VS Code" className="tech-icon-images" />
+        <SiVisualstudiocode size={45} color="#007ACC" />
         <div className="tech-icons-text">VS Code</div>
       </Col>
 
       <Col xs={6} md={3} lg={2} className="tech-icons">
-        <img src={visualStudio} alt="Visual Studio" className="tech-icon-images" />
-        <div className="tech-icons-text">Visual Studio</div>
-      </Col>
-
-      <Col xs={6} md={3} lg={2} className="tech-icons">
-        <img src={docker} alt="Docker" className="tech-icon-images" />
+        <SiDocker size={45} color="#2496ED" />
         <div className="tech-icons-text">Docker</div>
       </Col>
 
       <Col xs={6} md={3} lg={2} className="tech-icons">
-        <img src={Git} alt="Git" className="tech-icon-images" />
+        <SiGit size={45} color="#F05032" />
         <div className="tech-icons-text">Git</div>
       </Col>
 
       <Col xs={6} md={3} lg={2} className="tech-icons">
-        <img src={GitHub} alt="GitHub" className="tech-icon-images" />
+        <SiGithub size={45} />
         <div className="tech-icons-text">GitHub</div>
       </Col>
 
       <Col xs={6} md={3} lg={2} className="tech-icons">
-        <img src={Postman} alt="Postman" className="tech-icon-images" />
+        <SiPostman size={45} color="#FF6C37" />
         <div className="tech-icons-text">Postman</div>
-      </Col>
-
-      <Col xs={6} md={3} lg={2} className="tech-icons">
-        <img src={MySQL} alt="MySQL Workbench" className="tech-icon-images" />
-        <div className="tech-icons-text">MySQL Workbench</div>
       </Col>
 
     </Row>
